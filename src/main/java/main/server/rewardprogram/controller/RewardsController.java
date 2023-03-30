c0cfd9bf64e1d5328bc3ef363361596faa2a9c7c
Taron Hakobyan <taronhakobyan033@gmail.com>
2023-03-30 09:55:06 -0700

    samll change in RewadsController

--- src/main/java/main/server/rewardprogram/controller/RewardsController.java
+++ src/main/java/main/server/rewardprogram/controller/RewardsController.java
@@ -3,6 +3,7 @@ package main.server.rewardprogram.controller;
 import lombok.RequiredArgsConstructor;
 import main.server.rewardprogram.service.RewardService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.SQLException;
@@ -16,7 +17,7 @@ public class RewardsController {
     private final RewardService rewardService;
 
     @GetMapping("/rewards")
-    public Map<String, Integer> getPointCount(UUID customerID) throws SQLException {
+    public Map<String, Integer> getPointCount(@RequestParam UUID customerID) throws SQLException {
         return rewardService.calculateEarnedPointsForPast3Months(customerID);
     }
 
