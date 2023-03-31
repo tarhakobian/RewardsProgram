d844a62e10619ab177c8624fbc993c271672c1b9
Taron Hakobyan <taronhakobyan033@gmail.com>
2023-03-31 01:53:02 -0700

    Added some Comments

--- src/main/java/main/server/rewardprogram/service/RewardService.java
+++ src/main/java/main/server/rewardprogram/service/RewardService.java
@@ -21,12 +21,19 @@ public class RewardService {
     private final JdbcTemplate jdbcTemplate;
     private final PurchaseService purchaseService;
 
+    /** Short demonstration that calculateEarnedPointsForPast3MonthsByID(UUID uuid) works properly */
     public static void main(String[] args) throws SQLException {
         ConfigurableApplicationContext context = SpringApplication.run(RewardProgramApplication.class, args);
         RewardService service = context.getBean(RewardService.class);
         System.out.println(service.calculateEarnedPointsForPast3MonthsByID(UUID.fromString("11111111-1111-1111-1111-111111111111")));
     }
 
+    /** This function takes a UUID as input and calculates the total reward
+     *  points earned by that customer in the past 3 months, separated by month.
+     *  It does this by executing a SQL query using the JDBC template to sum the
+     *  reward points earned by the customer for each month. The results are then
+     *  stored in a Map with the month label as the key and the total reward points as the value.
+     */
     public Map<String, Integer> calculateEarnedPointsForPast3MonthsByID(UUID uuid) {
 
         Map<String, Integer> pointsMap = new HashMap<>();
@@ -51,6 +58,11 @@ public class RewardService {
         return pointsMap;
     }
 
+    /** This function calculates the total earned points for each customer for the past 3 months.
+     *  It does this by iterating over all purchases, and for each purchase,
+     *  it either creates a new entry in the result map for the customer and calculates
+     *  the earned points for the past 3 months, or merges the existing entry with the
+     *  calculated earned points for the purchase.*/
     public Map<UUID, Map<String, Integer>> calculateEarnedPointsForPast3MonthsForAllCustomers() {
         List<PurchaseResponse> purchases = purchaseService.getAll();
 
